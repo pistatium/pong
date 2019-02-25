56d356e6d486e2f589b571a932e31eb58ec009d0
Kimihiro N <kimihiro.nisimura@gmail.com>
2019-02-25 14:03:32 +0900

    :construction: WIP

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
+from typing import Tuple
+
 import pyxel
+
 
 WIDTH = 180
 HEIGHT = 120
+BALL_SIZE = 4
+FRICTION = 0.95
+
+
+class Obj:
+    size: int
+    friction: float
+    x: int
+    y: int
+    vx: float
+    vy: float
+
+    def __init__(self, size: int, friction: float, x: int, y: int):
+        self.size = size
+        self.friction = friction
+        self.x = x
+        self.y = y
+        self.vx = 0.0
+        self.vy = 0.0
+
+    def add_force(ax: float, ay: float):
+        self.vx += ax
+        self.vy += ay
+
+    def updated() -> :
+        self.x += self.vx
+        self.y += self.vy
+        self.vx *= self.friction
+        self.vy *= self.friction
 
 
 class Pong:

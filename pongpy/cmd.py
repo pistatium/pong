3461b231c4d451d2bf32cfdd3f75e04890ba9a20
Kimihiro N <kimihiro.nisimura@gmail.com>
2019-03-20 18:18:12 +0900

    :bug: Bug fix

--- pongpy/cmd.py
+++ pongpy/cmd.py
@@ -8,14 +8,10 @@ from pongpy.pong import PongMatch
 @click.command()
 @click.argument('team1', default='pongpy.teams.random_team:RandomTeam')
 @click.argument('team2', default='pongpy.teams.follow_team:FollowTeam')
-def cmd(team1, team2):
+def main(team1, team2):
     t1 = dynamic_import(team1)
     t2 = dynamic_import(team2)
     PongMatch(t1(), t2()).start()
-
-
-def main():
-    cmd()
 
 
 def dynamic_import(name):

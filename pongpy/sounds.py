77c1f1f8de103eb9fc5926ef8fc0ff82e9533ba5
TatchNicolas <TatchNicolas@users.noreply.github.com>
2019-09-14 17:04:25 +0900

    Add katsusha melody and base

--- pongpy/sounds.py
+++ pongpy/sounds.py
@@ -1,12 +1,11 @@
 from enum import Enum
 import pyxel
 
-# FIXME: このへんの構造よくわかってない
-#   ch: 独立して音楽再生できる空間?
-#   sound: 音を登録しておく場所? 
+
 class SoundChannel(Enum):
     SE = 0
     BGM = 1
+
 
 class Sound(Enum):
     LEFT_POINT = 1
@@ -15,8 +14,10 @@ class Sound(Enum):
     BGM_2 = 31
     BGM_3 = 32
 
+
 class Music(Enum):
     MAIN_BGM = 0
+
 
 def init_sounds():
     """
@@ -43,45 +44,56 @@ def init_sounds():
 
     # BGM
     # とりあえずサンプルを拝借
-    melody = (
-        "rrrr e3e3e3e3 d3d3c3c3 b2b2c3c3"
-        + "a2a2a2a2 c3c3c3c3 d3d3d3d3 e3e3e3e3"
-        + "rrrr e3e3e3e3 d3d3c3c3 b2b2c3c3"
-        + "a2a2a2a2 g2g2g2g2 c3c3c3c3 g2g2a2a2"
-        + "rrrr e3e3e3e3 d3d3c3c3 b2b2c3c3"
-        + "a2a2a2a2 c3c3c3c3 d3d3d3d3 e3e3e3e3"
-        + "f3f3f3a3 a3a3a3a3 g3g3g3b3 b3b3b3b3"
-        + "b3b3b3b4 rrrr e3d3c3g3 a2g2e2d2"
-    )
-    harmony1 = (
-        "a1 a1 a1 b1  f1 f1 c2 c2"
-        "c2 c2 c2 c2  g1 g1 b1 b1" * 3
-        + "f1 f1 f1 f1 f1 f1 f1 f1 g1 g1 g1 g1 g1 g1 g1 g1"
+    katyusha_melody1 = (
+        'd3d3d3d3 d3d3e3e3 f3f3f3f3 f3f3d3d3'
+        + 'f3f3f3f3 e3e3d3d3 e3e3e3e3 a2a2a2a2'
+        + 'e3e3e3e3 e3e3f3f3 g3g3g3g3 g3g3e3e3'
+        + 'g3g3g3g3 f3f3e3e3 d3d3d3d3 d3d3d3d3'
     )
 
-    harmony2 = (
-        ("f1" * 8 + "g1" * 8 + "a1" * 8 + ("c2" * 7 + "d2")) * 3 + "f1" * 16 + "g1" * 16
+    katyusha_melody2 = (
+        'a3a3a3a3 d4d4d4d4 c4c4c4c4 d4d4c4c4'
+        + 'b-3b-3b-3b-3 a3a3g3g3 a3a3a3a3 d3d3d3d3'
+        + 'rrb-3b-3 b-3b-3g3g3 a3a3a3a3 a3a3f3f3'
+        + 'g3g3g3g3 f3f3e3e3 d3d3d3d3 d3d3d3r'
+    )
+
+    katsusha_base = (
+        'd2d2a2a2 d2d2a2a2 d2d2a2a2 d2d2a2a2'
+        + 'd2d2a2a2 d2d2a2a2 a1a1c#2c#2 a1a1e2e2'
+        + 'a1a1c#2c#2 a1a1e2e2 a1a1c#2c#2 a1a1e2e2'
+        + 'a1a1e2e2 a1a1e2e2 d2d2a2a2 d2d2a2a2'
+    )
+
+    katsusha = katyusha_melody1
+
+    pyxel.sound(Sound.BGM_1.value).set(
+        note=katsusha,
+        tone="s",
+        volume=("3"),
+        effect=("n"),
+        speed=20,
     )
 
     pyxel.sound(Sound.BGM_2.value).set(
-        note=harmony1 * 2 + harmony2 * 2, tone="t", volume="5", effect="f", speed=20
-    )
-    
-    pyxel.sound(Sound.BGM_3.value).set(
-        note=("f0 r a4 r  f0 f0 a4 r" "f0 r a4 r   f0 f0 a4 f0"),
-        tone="n",
-        volume="6622 6622 6622 6426",
-        effect="f",
+        note=katsusha_base,
+        tone="s",
+        volume=("2"),
+        effect=("n"),
         speed=20,
     )
 
-    pyxel.sound(Sound.BGM_1.value).set(
-        note=melody,
-        tone="s",
-        volume=("3"),
-        effect=("nnnsffff"),
-        speed=20,
-    )
+    # pyxel.sound(Sound.BGM_2.value).set(
+    #     note=harmony1 * 2 + harmony2 * 2, tone="t", volume="5", effect="f", speed=20
+    # )
+    #
+    # pyxel.sound(Sound.BGM_3.value).set(
+    #     note=("f0 r a4 r  f0 f0 a4 r" "f0 r a4 r   f0 f0 a4 f0"),
+    #     tone="n",
+    #     volume="6622 6622 6622 6426",
+    #     effect="f",
+    #     speed=20,
+    # )
 
     ch0 = []
     ch1 = [Sound.BGM_1.value, ]
@@ -90,10 +102,10 @@ def init_sounds():
 
     pyxel.music(Music.MAIN_BGM.value).set(ch0, ch1, ch2, ch3)
 
+
 def play_se(se: Sound):
     pyxel.play(SoundChannel.SE.value, se.value)
 
 
 def play_bgm():
     pyxel.playm(Music.MAIN_BGM.value, loop=True)
-
